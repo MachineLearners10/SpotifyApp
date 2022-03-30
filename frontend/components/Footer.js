396d381caf1905dcefc76bf20fab6c8eba791981
Kathia Villavicencio <kathia.villavicencio1@outlook.com>
2022-03-29 23:23:11 -0500

    Fotter added

--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -1,55 +1,49 @@
 import React from "react";
-import { AppBar, Typography, makeStyles } from "@material-ui/core";
-import { ImageListItem } from "@material-ui/core";
-import { ImageList } from "@material-ui/core";
 
-const drawerWidth = 0;
-
-const useStyles = makeStyles((theme) => ({
-  appBar: {
-    top: "auto",
-    bottom: 0,
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: drawerWidth,
-    backgroundColor: "#000a12",
-    display: "flex",
-    justifyContent: "space-between",
-  },
-  footer1: {
-    marginLeft: 20,
-    fontSize: 17,
-  },
-  footer2: {
-    marginRight: 20,
-  },
-  image: {
-    width: 5,
-    height: 5,
-  },
-}));
-
-function Footer() {
-  const classes = useStyles();
+const Footer = () => {
   return (
-    <AppBar position="fixed" className={classes.appBar}>
-      <Typography variant="h6" className={classes.footer1}>
-        SpotifyApp
-      </Typography>
-      {/* <ImageList
-      // className={classes.footer2}
-      // sx={{ width: 10, height: 10 }}
-      // cols={5}
-      // rowHeight={16}
-      >
-        <ImageListItem className={classes.image}>
-          <img src={require("./Images/SpotifyIcon.png")} alt="" />
-          <img src={require("./Images/GithubIcon.png")} alt="" />
-          <img src={require("./Images/FacebookIcon.png")} alt="" />
-          <img src={require("./Images/InstagramIcon.png")} alt="" />
-        </ImageListItem>
-      </ImageList> */}
-    </AppBar>
+    <footer className="footer">
+      <div className="footer-icons">
+        <a href="https://www.spotify.com/us/">
+          <img
+            className="footer-icon"
+            src={
+              "https://1000logos.net/wp-content/uploads/2017/08/Spotify-emblem.jpg"
+            }
+            alt=""
+          />
+        </a>
+        <a href="https://github.com/MachineLearners10/SpotifyApp">
+          <img
+            className="footer-icon"
+            src={
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn11Rru16VFClaKhYzoL9yGLIwOEaggMpa1Q&usqp=CAU"
+            }
+            alt=""
+          />
+        </a>
+        <a href="https://www.facebook.com/">
+          <img
+            className="footer-icon"
+            src={
+              "https://www.pinclipart.com/picdir/big/150-1504080_facebook-white-facebook-white-icon-png-2018-clipart.png"
+            }
+            alt=""
+          />
+        </a>
+        <a href="https://www.instagram.com/">
+          <img
+            className="footer-icon"
+            src={
+              "https://www.citypng.com/public/uploads/preview/-51609193615decnmgwyz7.png"
+            }
+            alt=""
+          />
+        </a>
+        <div className="footer-title">Catch A Vibe</div>
+      </div>
+    </footer>
   );
-}
+};
 
 export default Footer;
